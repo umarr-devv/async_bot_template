298ab347a561bb848326a96813af77b0fc6832da
umarr <umarr.developer@gmail.com>
2024-09-11 20:58:18 +0600

    fix on_start handler

--- src/handlers/private/user/start.py
+++ src/handlers/private/user/start.py
@@ -4,7 +4,7 @@ from aiogram.filters import CommandStart, CommandObject
 router = Router()
 
 
-@router.message(CommandStart(deep_link=True))
+@router.message(CommandStart())
 async def on_start(message: types.Message, command: CommandObject):
     text = f'Добро пожаловать, {message.from_user.full_name}'
     await message.answer(text=text)
