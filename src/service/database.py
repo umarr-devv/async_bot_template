69b1cdf9405893a566c13a966a72b471f1f0a248
umarr <umarr.developer@gmail.com>
2024-09-12 18:29:33 +0600

    edit DataBase class, remove get_session method

--- src/service/database.py
+++ src/service/database.py
@@ -18,14 +18,9 @@ class DataBase:
         self.engine = create_async_engine(
             url=self.url
         )
-        self.session_factory = async_sessionmaker(
+        self.sessions = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False
         )
-
-    async def get_session(self) -> AsyncSession:
-        async with self.session_factory() as session:
-            yield session
-            await session.close()
