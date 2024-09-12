16e062140423bc18ec26bb00d1d5c189b2cbafc2
umarr <umarr.developer@gmail.com>
2024-09-12 18:37:45 +0600

    delete invalid imports

--- src/handlers/private/user/start.py
+++ src/handlers/private/user/start.py
@@ -1,9 +1,6 @@
 from aiogram import types, Router
 from aiogram.filters import CommandStart, CommandObject
 
-from src.models.crud import create_user
-from src.service.database import DataBase
-
 router = Router()
 
 
