6945f8053bbc60786c5dc7bee61f489cb7801771
umarr <umarr.developer@gmail.com>
2024-10-11 22:28:32 +0600

    edit set_commands function

--- src/utils/commands.py
+++ src/utils/commands.py
@@ -1,15 +1,19 @@
 from aiogram import types, Bot
 
+COMMANDS = [
+    types.BotCommand(
+        command='start',
+        description='Запустить бота'
+    ),
+    types.BotCommand(
+        command='help',
+        description='Помощь'
+    )
+]
+
 
 async def set_commands(bot: Bot):
     await bot.set_my_commands(
-        commands=[
-            types.BotCommand(
-                command='start', description='Запустить бота'
-            ),
-            types.BotCommand(
-                command='help', description='Помощь'
-            )
-        ],
+        commands=COMMANDS,
         scope=types.BotCommandScopeAllPrivateChats()
     )
