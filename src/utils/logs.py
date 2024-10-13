007b81acb7556f7acb57c759e902d671bf7cbcbf
umarr <umarr.developer@gmail.com>
2024-10-13 17:38:59 +0600

    update logging tools

--- src/utils/logs.py
+++ src/utils/logs.py
@@ -1,17 +1,25 @@
-import logging
+import os
+from logging import StreamHandler, basicConfig
+from logging.handlers import TimedRotatingFileHandler
 
 from src.config import Config
 
-LOG_FORMAT = "%(name)s | %(asctime)s | %(funcName)s | %(filename)s | %(levelname)s | %(message)s"
-LOG_FILE = '.log'
+LOG_FORMAT = '%(name)s | %(asctime)s | %(funcName)s | %(filename)s | %(levelname)s | %(message)s'
+LOG_FILENAME = '.log'
+LOG_DIR = os.getcwd() + '/logs/'
 
 
 def set_logging(config: Config):
-    logging.basicConfig(
+    basicConfig(
         level=config.logging.level,
         format=LOG_FORMAT,
         handlers=[
-            logging.StreamHandler(),
-            logging.FileHandler(filename=LOG_FILE, encoding='utf-8')
+            StreamHandler(),
+            TimedRotatingFileHandler(
+                filename=LOG_DIR + LOG_FILENAME,
+                when='S',
+                interval=20,
+                encoding='utf-8'
+            )
         ]
     )
