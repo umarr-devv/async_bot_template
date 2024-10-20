c6a77ae3970c2e2fc55b5937f7f405e230d2dd00
umarr <umarr.developer@gmail.com>
2024-10-20 20:43:28 +0600

    fix database.url

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app import Base, database
+from app import Base, config as conf
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +22,8 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-config.set_main_option("sqlalchemy.url", database.url)
+config.set_main_option("sqlalchemy.url", conf.db.url)
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
