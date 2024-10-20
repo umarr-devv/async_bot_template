d7bbb414a8e7999f14f7d16778c84b2c4b96bfe7
umarr <umarr.developer@gmail.com>
2024-10-20 20:45:52 +0600

    update user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 
-from sqlalchemy import (BigInteger, Column, Date, Integer)
+from sqlalchemy import (Date, Integer)
+from sqlalchemy.orm import mapped_column, Mapped
 
 from src.service.database import Base
+from src.utils.funcs import current_utc_time
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(BigInteger, unique=True, nullable=False)
-    created_on = Column(Date, default=datetime.now)
-    update_on = Column(Date, default=datetime.now, onupdate=datetime.now)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
+    created_on: Mapped[datetime] = mapped_column(Date, default=current_utc_time)
+    updated_on: Mapped[datetime] = mapped_column(Date, default=current_utc_time,
+                                                 onupdate=current_utc_time)
